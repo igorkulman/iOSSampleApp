24682c76169662fbcca7a1afa503d6ce1cd51719
Igor Kulman <igor@kulman.sk>
2017-10-04 12:07:58 +0200

    + adding unit tests for the data service

---
+++ iOSSampleAppTests/DataServiceTests.swift
@@ -0,0 +1,38 @@
+//
+//  DataServiceTests.swift
+//  iOSSampleAppTests
+//
+//  Created by Igor Kulman on 04/10/2017.
+//  Copyright © 2017 Igor Kulman. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import Nimble
+@testable import iOSSampleApp
+
+class DataServiceTests: XCTestCase {
+    
+    private let service = DataService()
+    
+    func testParsingValidFeed() {
+        let source = RssSource(title: "Coding Journal", url: "https://blog.kulman.sk", rss: "https://blog.kulman.sk/index.xml", icon: nil)
+        waitUntil(timeout: 5) { done in
+            self.service.getFeed(source: source) { items in
+                expect(items).notTo(beEmpty())
+                done()
+            }
+        }
+    }
+    
+    func testParsingInvalidValidFeed() {
+        let source = RssSource(title: "Fake", url: "", rss: "", icon: nil)
+        waitUntil(timeout: 5) { done in
+            self.service.getFeed(source: source) { items in
+                expect(items).to(beEmpty())
+                done()
+            }
+        }
+    }
+    
+}
