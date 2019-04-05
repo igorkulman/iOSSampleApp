7eb729aa1e752a88edf178c047201dca1d2b0e3f
Igor Kulman <igor@kulman.sk>
2019-04-05 16:59:07 +0200

    📖 mentioning XcodeGen in README

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@ brew update
 brew ls --versions carthage && brew upgrade carthage || brew install carthage
 brew ls --versions swiftlint && brew upgrade swiftlint || brew install swiftlint
 brew ls --versions swiftgen && brew upgrade swiftgen || brew install swiftgen
+brew ls --versions xcodegen && brew upgrade xcodegen || brew install xcodegen
 sudo gem install fastlane -NV
 
 fastlane carthage_bootstrap
