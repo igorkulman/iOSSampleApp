448fb8d38f37dc5fb9c2c9e394c598c8efe78e0e
Igor Kulman <igor@kulman.sk>
2020-04-11 11:54:16 +0200

    ✨ adding a way to log string with os_log

--- Sources/iOSSampleApp/Supporting Files/Extensions/OSLog+Extensions.swift
+++ Sources/iOSSampleApp/Supporting Files/Extensions/OSLog+Extensions.swift
@@ -13,5 +13,10 @@ extension OSLog {
     private static var subsystem = Bundle.main.bundleIdentifier!
 
     static let data = OSLog(subsystem: subsystem, category: "data")
+    static let conditions = OSLog(subsystem: subsystem, category: "conditions")
     static let lifeCycle = OSLog(subsystem: subsystem, category: "lifeCycle")
+
+    static func log(_ message: String, log: OSLog = .default, type: OSLogType = .default) {
+        os_log("%@", log: log, type: type, message)
+    }
 }
