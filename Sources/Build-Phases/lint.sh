56b4c63df92b3034e02a7f5e9d55770f392f1cee
Igor Kulman <igor@kulman.sk>
2023-04-01 09:30:38 +0200

    🔧 Use SwiftLint as Xcode build plugin

--- Sources/Build-Phases/lint.sh
+++
@@ -1,6 +0,0 @@
-export PATH="$PATH:/opt/homebrew/bin"
-if which swiftlint >/dev/null; then
-swiftlint
-else
-echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
-fi
