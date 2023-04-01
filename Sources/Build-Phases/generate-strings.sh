f3cdaaac7a5ee463990188bf02f1479ebb103565
Igor Kulman <igor@kulman.sk>
2023-04-01 09:41:55 +0200

    🔧 Use SwiftGen as Xcode build plugin

--- Sources/Build-Phases/generate-strings.sh
+++
@@ -1,6 +0,0 @@
-export PATH="$PATH:/opt/homebrew/bin"
-if which swiftgen >/dev/null; then
-swiftgen
-else
-echo "warning: SwiftGen not installed, download from https://github.com/SwiftGen/SwiftGen"
-fi
