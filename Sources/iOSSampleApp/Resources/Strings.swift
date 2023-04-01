82cc38410218a6b4d8158c5bb3cc680dc4d3b440
Igor Kulman <igor@kulman.sk>
2023-04-01 09:41:55 +0200

    🔧 Use SwiftGen as Xcode build plugin

--- Sources/iOSSampleApp/Resources/Strings.swift
+++ Sources/iOSSampleApp/Resources/Strings.swift
@@ -3,7 +3,7 @@
 
 import Foundation
 
-// swiftlint:disable superfluous_disable_command file_length implicit_return
+// swiftlint:disable superfluous_disable_command file_length implicit_return prefer_self_in_static_references
 
 // MARK: - Strings
 
@@ -11,41 +11,45 @@ import Foundation
 // swiftlint:disable nesting type_body_length type_name vertical_whitespace_opening_braces
 internal enum L10n {
   /// About
-  internal static let about = L10n.tr("Localizable", "about")
+  internal static let about = L10n.tr("Localizable", "about", fallback: "About")
   /// Add custom
-  internal static let addCustom = L10n.tr("Localizable", "add_custom")
+  internal static let addCustom = L10n.tr("Localizable", "add_custom", fallback: "Add custom")
   /// Add custom source
-  internal static let addCustomSource = L10n.tr("Localizable", "add_custom_source")
+  internal static let addCustomSource = L10n.tr("Localizable", "add_custom_source", fallback: "Add custom source")
   /// About author
-  internal static let author = L10n.tr("Localizable", "author")
+  internal static let author = L10n.tr("Localizable", "author", fallback: "About author")
   /// Back
-  internal static let back = L10n.tr("Localizable", "back")
+  internal static let back = L10n.tr("Localizable", "back", fallback: "Back")
   /// Invalid RSS feed URL
-  internal static let badUrl = L10n.tr("Localizable", "bad_url")
+  internal static let badUrl = L10n.tr("Localizable", "bad_url", fallback: "Invalid RSS feed URL")
   /// Author's blog
-  internal static let blog = L10n.tr("Localizable", "blog")
+  internal static let blog = L10n.tr("Localizable", "blog", fallback: "Author's blog")
   /// Done
-  internal static let done = L10n.tr("Localizable", "done")
+  internal static let done = L10n.tr("Localizable", "done", fallback: "Done")
   /// Feed
-  internal static let feed = L10n.tr("Localizable", "feed")
+  internal static let feed = L10n.tr("Localizable", "feed", fallback: "Feed")
   /// Used libraries
-  internal static let libraries = L10n.tr("Localizable", "libraries")
+  internal static let libraries = L10n.tr("Localizable", "libraries", fallback: "Used libraries")
   /// Logo URL
-  internal static let logoUrl = L10n.tr("Localizable", "logo_url")
+  internal static let logoUrl = L10n.tr("Localizable", "logo_url", fallback: "Logo URL")
   /// Network problem has occured
-  internal static let networkProblem = L10n.tr("Localizable", "network_problem")
+  internal static let networkProblem = L10n.tr("Localizable", "network_problem", fallback: "Network problem has occured")
   /// Optional
-  internal static let `optional` = L10n.tr("Localizable", "optional")
+  internal static let `optional` = L10n.tr("Localizable", "optional", fallback: "Optional")
   /// Pull to refresh
-  internal static let pullToRefresh = L10n.tr("Localizable", "pull_to_refresh")
+  internal static let pullToRefresh = L10n.tr("Localizable", "pull_to_refresh", fallback: "Pull to refresh")
   /// RSS URL
-  internal static let rssUrl = L10n.tr("Localizable", "rss_url")
-  /// Select source
-  internal static let selectSource = L10n.tr("Localizable", "select_source")
+  internal static let rssUrl = L10n.tr("Localizable", "rss_url", fallback: "RSS URL")
+  /// Localizable.strings
+  ///  iOSSampleApp
+  ///  
+  ///  Created by Igor Kulman on 03/10/2017.
+  ///  Copyright © 2017 Igor Kulman. All rights reserved.
+  internal static let selectSource = L10n.tr("Localizable", "select_source", fallback: "Select source")
   /// Title
-  internal static let title = L10n.tr("Localizable", "title")
+  internal static let title = L10n.tr("Localizable", "title", fallback: "Title")
   /// Url
-  internal static let url = L10n.tr("Localizable", "url")
+  internal static let url = L10n.tr("Localizable", "url", fallback: "Url")
 }
 // swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:enable nesting type_body_length type_name vertical_whitespace_opening_braces
@@ -53,8 +57,8 @@ internal enum L10n {
 // MARK: - Implementation Details
 
 extension L10n {
-  private static func tr(_ table: String, _ key: String, _ args: CVarArg...) -> String {
-    let format = BundleToken.bundle.localizedString(forKey: key, value: nil, table: table)
+  private static func tr(_ table: String, _ key: String, _ args: CVarArg..., fallback value: String) -> String {
+    let format = BundleToken.bundle.localizedString(forKey: key, value: value, table: table)
     return String(format: format, locale: Locale.current, arguments: args)
   }
 }
